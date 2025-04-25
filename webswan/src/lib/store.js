6ca7cd82fa5a7e9784fcdb0c6d9aff876669c670
bilal-el-akrouchi <bilalelakrouchi@gmail.com>
2025-04-25 16:49:23 +0200

    début de projet

---
+++ webswan/src/lib/store.js
@@ -0,0 +1,10 @@
+// src/lib/store.js
+'use client'
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './slices/counterSlice'   // ← ou './slices/counerSlice' si tu ne renommes pas
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer     // state.counter.value …
+  }
+})
