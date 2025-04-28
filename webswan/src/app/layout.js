7645703a1052121aca537e7a696bcd96d1abf428
Ali <zallitoaliinfo@gmail.com>
2025-04-28 14:34:32 +0200

    installation sass

--- webswan/src/app/layout.js
+++ webswan/src/app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "./components/navbar/Navbar"
 import Footer from "./components/footer/Footer"
 import StoreProvider from "./StoreProvider";
-import "./globals.css";
+import "./globals.sass";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
