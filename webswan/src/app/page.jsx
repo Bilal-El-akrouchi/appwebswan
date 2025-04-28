24cdb29f3f34df4979656c056eb7b391026a6970
bilal-el-akrouchi <bilalelakrouchi@gmail.com>
2025-04-28 15:27:16 +0200

    ajouter navbarr

--- webswan/src/app/page.jsx
+++ webswan/src/app/page.jsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import logo from '../../public/img/'
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <header className="bg-custom w-full h-screen">
+        <nav className="w-full h-[10vh] bg-custom-deux shadow-xl">
+          <h1><img src={logo} alt="web swan logo" /></h1>
+        </nav>
+      </header>
+    </>
   );
 }
