4df12789f03846c43014db3b8b3f7ac32c3829b4
bilal-el-akrouchi <bilalelakrouchi@gmail.com>
2025-05-05 16:41:35 +0200

    ajouter header

--- webswan/src/app/page.jsx
+++ webswan/src/app/page.jsx
@@ -1,10 +1,9 @@
 "use client";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import logo from './../../public/img/logo.png'
-import cygne from './../../public/img/header_cygne.png'
+import logo from "./../../public/img/logo.png";
+import cygndpng from "./../../public/img/cygne_sans_fond.png";
 // import useAuth from "@/hooks/useAuth";
-
 
 export default function Home() {
   const [zoom, setZoom] = useState(null);
@@ -21,13 +20,12 @@ export default function Home() {
   ]);
 
   const supp = (index) => {
-    setTimeLeft(4)
+    setTimeLeft(4);
     setAuto(true);
     if (index) {
       setZoom(index);
-      
-    }else {
-      setZoom(0)
+    } else {
+      setZoom(0);
     }
     if (forfait === 4) {
       setForfait(1);
@@ -69,120 +67,137 @@ export default function Home() {
   }, [timeLeft]);
 
   const autoBar = {
-    width: `${timeLeft*25}%`
-  }
+    width: `${timeLeft * 25}%`,
+  };
 
   return (
     <>
-    
-    <header className="bg-custom w-full h-screen">
-        <nav className="w-full h-[10vh] bg-custom-deux shadow-xl flex justify-around items-center">
-          {/* Logo : width/height requis par <Image /> pour éviter l'erreur de compilation Next.js */}
-          <Image
-            src={logo}
-            alt="webswan logo"
-            width={120}
-            height={60}
-            className="w-[5vw] h-auto"
-            priority
-          />
-
-          <div className="flex items-center gap-11">
-            <input
-              type="text"
-              placeholder="connexion"
-              className="flex justify-center items-center rounded-3xl bg-custom w-[100px] h-[50px] "
-            />
-            <button
-              // onClick={handleSignInClick}
-              className="bg-custom p-2 cursor-pointer"
-            >
-              sign in
-            </button>
+      <header className="bg-custom w-full h-screen">
+        <div className="absolute flex justify-center items-center pt-17 w-[100%] ">
+          <div className="flex justify-end  item-center 0 opacity-70 w-screen ">
+            <h1 className="text-white text-[11rem] ">MODEL </h1>
           </div>
-        </nav>
-        <div>
-          <h1 className="text-white ">Swan Brut</h1>
+          <div className="flex justify-start item-center  opacity-70 z-10  w-screen">
+            <h1 className="text-white text-[11rem] "> BRUT </h1>
+          </div>
         </div>
 
+        <div className="enfantheader absolute">
+          <nav className="w-full h-[10vh] bg-custom-deux shadow-xl flex justify-around items-center">
+            {/* Logo : width/height requis par <Image /> pour éviter l'erreur de compilation Next.js */}
+            <Image
+              src={logo}
+              alt="webswan logo"
+              width={120}
+              height={60}
+              className="w-[5vw] h-auto"
+              priority
+            />
+
+            <div className="flex items-center gap-11">
+              <input
+                type="text"
+                placeholder="connexion"
+                className="flex justify-center items-center rounded-3xl bg-custom w-[100px] h-[50px] "
+              />
+              <button
+                // onClick={handleSignInClick}
+                className="bg-custom p-2 cursor-pointer"
+              >
+                sign in
+              </button>
+            </div>
+          </nav>
+          <div></div>
+        </div>
       </header>
 
-    <div id="home">
-      <section className={`${bg} overflow-hidden w-full`}>
-        <div style={autoBar} className={`w-full h-5 transition-all ${forfait == 0 ? '' : `${items[0]}`}`}></div>
-        <div
-          className={`m-30 absolute z-10 ${forfait == 0 ? "opacity-100" : "opacity-0"
+      <div id="home">
+        <section className={`${bg} overflow-hidden w-full`}>
+          <div
+            style={autoBar}
+            className={`w-full h-5 transition-all ${
+              forfait == 0 ? "" : `${items[0]}`
             }`}
-        >
-          <p className="text-5xl">Nos différents forfait</p>
-          <div className="mt-30">
-
-            <p>Forfait 1</p>
-            <p>Forfait 2</p>
-            <p>Forfait 3</p>
-            <p>Forfait 4</p>
+          ></div>
+          <div
+            className={`m-30 absolute z-10 ${
+              forfait == 0 ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <p className="text-5xl">Nos différents forfait</p>
+            <div className="mt-30">
+              <p>Forfait 1</p>
+              <p>Forfait 2</p>
+              <p>Forfait 3</p>
+              <p>Forfait 4</p>
+            </div>
           </div>
-        </div>
-        <div
-          className={`m-30 absolute z-10 transition ${forfait == 1 ? "opacity-100" : "opacity-0"
+          <div
+            className={`m-30 absolute z-10 transition ${
+              forfait == 1 ? "opacity-100" : "opacity-0"
             }`}
-        >
-          <p className="text-5xl">Forfait 1</p>
-          <p className="w-1/2 mt-30">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-            consequatur soluta quod voluptatem mollitia? Provident est
-            voluptatibus hic minus tempore.
-          </p>
-        </div>
-        <div
-          className={`m-30 absolute z-10 transition ${forfait == 2 ? "opacity-100" : "opacity-0"
+          >
+            <p className="text-5xl">Forfait 1</p>
+            <p className="w-1/2 mt-30">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+              consequatur soluta quod voluptatem mollitia? Provident est
+              voluptatibus hic minus tempore.
+            </p>
+          </div>
+          <div
+            className={`m-30 absolute z-10 transition ${
+              forfait == 2 ? "opacity-100" : "opacity-0"
             }`}
-        >
-          <p className="text-5xl">Forfait 2</p>
-          <p className="w-1/2 mt-30">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-            consequatur soluta quod voluptatem mollitia? Provident est
-            voluptatibus hic minus tempore.
-          </p>
-        </div>
-        <div
-          className={`m-30 absolute z-10 transition ${forfait == 3 ? "opacity-100" : "opacity-0"
+          >
+            <p className="text-5xl">Forfait 2</p>
+            <p className="w-1/2 mt-30">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+              consequatur soluta quod voluptatem mollitia? Provident est
+              voluptatibus hic minus tempore.
+            </p>
+          </div>
+          <div
+            className={`m-30 absolute z-10 transition ${
+              forfait == 3 ? "opacity-100" : "opacity-0"
             }`}
-        >
-          <p className="text-5xl">Forfait 3</p>
-          <p className="w-1/2 mt-30">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-            consequatur soluta quod voluptatem mollitia? Provident est
-            voluptatibus hic minus tempore.
-          </p>
-        </div>
-        <div
-          className={`m-30 absolute z-10 transition ${forfait == 4 ? "opacity-100" : "opacity-0"
+          >
+            <p className="text-5xl">Forfait 3</p>
+            <p className="w-1/2 mt-30">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+              consequatur soluta quod voluptatem mollitia? Provident est
+              voluptatibus hic minus tempore.
+            </p>
+          </div>
+          <div
+            className={`m-30 absolute z-10 transition ${
+              forfait == 4 ? "opacity-100" : "opacity-0"
             }`}
-        >
-          <p className="text-5xl">Forfait 4</p>
-          <p className="w-1/2 mt-30">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-            consequatur soluta quod voluptatem mollitia? Provident est
-            voluptatibus hic minus tempore.
-          </p>
-        </div>
+          >
+            <p className="text-5xl">Forfait 4</p>
+            <p className="w-1/2 mt-30">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+              consequatur soluta quod voluptatem mollitia? Provident est
+              voluptatibus hic minus tempore.
+            </p>
+          </div>
 
-        <div className="h-screen ml-50 w-full flex gap-2 items-end justify-end">
-          {items.map((item, index) => (
-            <div
-              onClick={() => {
-                supp(index);
-              }}
-              key={index}
-              className={`h-[50%] w-55 rounded-xl z-10 transition ${item} ${zoom == index ? "zoom" : ""
+          <div className="h-screen ml-50 w-full flex gap-2 items-end justify-end">
+            {items.map((item, index) => (
+              <div
+                onClick={() => {
+                  supp(index);
+                }}
+                key={index}
+                className={`h-[50%] w-55 rounded-xl z-10 transition ${item} ${
+                  zoom == index ? "zoom" : ""
                 }`}
-            ></div>
-          ))}
-        </div>
-      </section>
-    </div>
+              ></div>
+            ))}
+          </div>
+        </section>
+      </div>
     </>
-// >>>>>>> bc3118eba67ce14bf4e8fbb41a0758d65d7dc369
+    // >>>>>>> bc3118eba67ce14bf4e8fbb41a0758d65d7dc369
   );
 }
